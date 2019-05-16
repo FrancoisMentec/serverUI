812919909b71fa3a1ab42ba9102cfe10e94ced0b
Francois <mentec.francois@gmail.com>
2019-05-16 17:29:01 +0200

    fix dir content

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.post('/directory-content', (req, res) => {
       } else {
         let f = []
         for (let i = 0, li = files.length; i < li; i++) {
-          if (files[i].name.length > 0 && files[i].name !== '.' && files[i].name !== '..') {
+          if (typeof files[i].name === 'string' && files[i].name.length > 0 && files[i].name !== '.' && files[i].name !== '..') {
             let filePath = path.join(req.body.path, files[i].name)
             let fd = {
               name: files[i].name,
